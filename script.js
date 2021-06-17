c621c2f08ca5eba454abbba71f8696541a08e8b7
benjamin <benjamin.olmsted@gmail.com>
2021-06-17 15:31:26 -0700

    cleanup

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const h2 = document.createElement('h2');
-h2.textContent = "by benjamin olmsted";
-document.querySelector('body').appendChild(h2);
+
 let nPara = 0;
 const paras = document.querySelectorAll('p');
 
